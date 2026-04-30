ed4eb2cb25180394c88634293d7aa996c559cd1d
KristoveNohy <97287079+KristoveNohy@users.noreply.github.com>
2026-04-30 10:19:46 +0200

    npx tsc -p backend/tsconfig.json --noEmit bez errorrov

---
+++ backend/shared/nodemailer.d.ts
@@ -0,0 +1,37 @@
+declare module "nodemailer" {
+  namespace nodemailer {
+    interface TransportOptions {
+      host?: string;
+      port?: number;
+      secure?: boolean;
+      ignoreTLS?: boolean;
+      requireTLS?: boolean;
+      auth?: {
+        user: string;
+        pass: string;
+      };
+      tls?: {
+        rejectUnauthorized?: boolean;
+      };
+    }
+
+    interface SendMailOptions {
+      from?: string;
+      to?: string;
+      subject?: string;
+      text?: string;
+    }
+
+    interface Transporter {
+      sendMail(options: SendMailOptions): Promise<unknown>;
+    }
+  }
+
+  function createTransport(options: nodemailer.TransportOptions): nodemailer.Transporter;
+
+  const nodemailer: {
+    createTransport: typeof createTransport;
+  };
+
+  export = nodemailer;
+}
